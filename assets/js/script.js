51c9b4daf91c4fdc1deeaf4fea912711b73ac749
lreshmaprdxn <reshmal@prdxn.com>
2020-02-24 19:43:28 +0530

    js modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ var hamburger = document.querySelector('.hamburger');
 var nav = document.querySelector('.menu');
 hamburger.addEventListener('click',navshow);
 
-
 function navshow() {
   if (nav.style.display === "block") {
     nav.style.display = "none";

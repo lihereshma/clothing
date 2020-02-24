748956f1e3a2f0f29969b1a46a4116893e5dcf84
lreshmaprdxn <reshmal@prdxn.com>
2020-02-24 19:10:31 +0530

    js end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-/* Author: 
+var hamburger = document.querySelector('.hamburger');
+var nav = document.querySelector('.menu');
+hamburger.addEventListener('click',navshow);
 
-*/
 
+function navshow() {
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+  hamburger.classList.toggle('open');
+}
 
+$(document).ready(function(){
+  $('.slider').slick({
+  slidesToShow: 1,
+  dots:true,
+  autoplay:true
+  });
+});
 
 
 
